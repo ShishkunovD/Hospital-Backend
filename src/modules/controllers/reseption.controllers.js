820c35d8adf5c8982de466100aeaee6a1cace76f
Dmitriy <dshishkunov.exceedteam@gmail.com>
2022-01-21 19:44:24 +0300

    add editing reseption

--- src/modules/controllers/reseption.controllers.js
+++ src/modules/controllers/reseption.controllers.js
@@ -23,3 +23,20 @@ module.exports.createNewReseption = (req, res) => {
     res.status(404).send("Error, all fields should be filled in correctly.");
   }
 }
+
+module.exports.updateReseption = (req, res) => {
+  if (req.query.id !== undefined && 
+    (req.body.name !== undefined && req.body.doctor !== undefined && req.body.date !== undefined && req.body.complaints !== undefined)) {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, secret);
+    const param = req.query.id;
+    const body = req.body;
+    Reseption.findByIdAndUpdate(param, body).then(result => {
+      Reseption.find({ owner: decoded.userId }).then(result => {
+        res.send({ data: result });
+      });
+    });
+  } else {
+    res.status(404).send("Error, all fields should be filled in correctly");
+  }
+}
--- src/modules/routes/reseption.routes.js
+++ src/modules/routes/reseption.routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const {
   getAllReseption,
-  createNewReseption
+  createNewReseption,
+  updateReseption
 } = require('../controllers/reseption.controllers');
 
 router.get('/getAllReseption', getAllReseption);
 router.post('/createReseption', createNewReseption);
+router.patch('/updateReseption', updateReseption);
 
 module.exports = router;
